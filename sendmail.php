cd27b15ac401d4c8090c4d72705794dd65a3d011
inne <mrbelongtim@gmail.com>
2023-11-28 16:02:35 +0100

    fixed capcha check

--- sendmail.php
+++ sendmail.php
@@ -8,15 +8,13 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-// Подключение к Google reCAPTCHA
-$recaptchaSecretKey = '6LfgRdQoAAAAAPrv7_2Du6rwzZMspqh-a1pXryGV';
-$recaptchaResponse = isset($_POST['recaptchaToken']) ? $_POST['recaptchaToken'] : null;
+$recaptchaSecretKey = '6LfgRdQoAAAAAPbcIgYjRxMBJM16uwst97CkmpLJ';
+$recaptchaResponse = isset($_POST['g-recaptcha-response']) ? $_POST['g-recaptcha-response'] : null;
 
 $recaptcha = new ReCaptcha\ReCaptcha($recaptchaSecretKey);
 $resp = $recaptcha->verify($recaptchaResponse);
 
 if ($resp->isSuccess()) {
-    // Если reCAPTCHA успешно пройдена (действительно?))
     $mail = new PHPMailer(true);
     $mail->SMTPDebug = 0;
     $mail->CharSet = 'UTF-8';
@@ -24,7 +22,7 @@ if ($resp->isSuccess()) {
     $mail->SMTPAuth = true;
 
     $from = 'info@sterkanazed.cz';
-    $namefrom = 'Novacolor';
+    $namefrom = 'sterkanazed.cz';
     $mail->Host = "server303.web-hosting.com";
     $mail->Port = 465;
     $mail->Username = $from;
@@ -32,44 +30,40 @@ if ($resp->isSuccess()) {
     $mail->SMTPSecure = "ssl";
 
     $mail->setFrom($from, $namefrom);
-    $mail->Subject = 'Zpětná vazba z formuláře od: ' . $namefrom;
+    $mail->Subject = 'Новое письмо с сайта: ' . $namefrom;
     $mail->isHTML();
-    $mail->addAddress('mrbelongtim@gmail.com', 'Admin 1');
-    $mail->addCC('inne2022@icloud.com', 'Admin 2');
+    $mail->addAddress('info@sterkanazed.cz', 'Admin 1');
+    $mail->addCC('novacolor@novacolordesign.cz', 'Admin 2');
 
-    $body = '<h1>Nové upozornění</h1>';
+    $body = '<h1>Новый запрос</h1>';
 
     $name = isset($_POST['name']) ? $_POST['name'] : null;
-    $email = isset($_POST['email']) ? $_POST['email'] : null;
     $phone = isset($_POST['phone']) ? $_POST['phone'] : null;
     $message = isset($_POST['message']) ? $_POST['message'] : null;
 
     if (!empty($name)) {
-        $body .= '<p><strong>Jmeno: </strong>' . trim($name) . '</p>';
-    }
-
-    if (!empty($email)) {
-        $body .= '<p><strong>E-mail: </strong>' . trim($email) . '</p>';
-
-        $mail->addAddress($email, $name);
+        $body .= '<p><strong>Имя: </strong>' . trim($name) . '</p>';
     }
 
     if (!empty($phone)) {
-        $body .= '<p><strong>Telefonní číslo: </strong>' . trim($phone) . '</p>';
+        $body .= '<p><strong>Телефонный номер: </strong>' . trim($phone) . '</p>';
     }
 
     if (!empty($message)) {
-        $body .= '<p><strong>Text: </strong>' . $message . '</p>';
+        $body .= '<p><strong>Сообщение: </strong>' . $message . '</p>';
     }
 
     $mail->Body = $body;
 
-    $result = $mail->send();
-
-    $response = ['message' => $message, 'result' => $result];
+    try {
+        $result = $mail->send();
+        $response = ['message' => $message, 'result' => $result];
+    } catch (Exception $e) {
+        $response = ['message' => $e->getMessage(), 'result' => false];
+    }
 } else {
     // Если reCAPTCHA не пройдена
-    $response = ['error' => 'reCAPTCHA verification failed'];
+    $response = ['error' => $resp->getErrorCodes()];
 }
 
 header('Content-type: application/json');
